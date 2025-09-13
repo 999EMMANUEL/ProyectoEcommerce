015e2afdfa4b711c1929d5b4426efb0fcdb6abda
JoanCR1221 <jandree1221@gmail.com>
2025-09-12 21:04:25 -0600

    cliente

---
+++ ProyectoEcommerce/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyectoEcommerce.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+    }
+}

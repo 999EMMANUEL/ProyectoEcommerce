6d433305b2a75ea5939b06f7ac6b064eb5534f36
Jhon Alonso López zuñiga <jhon56luzu@gmail.com>
2025-09-12 20:29:14 -0600

    Añadir controlador y vistas para gestión de productos
    Se ha implementado el controlador `ProductsController` para manejar las operaciones CRUD de productos utilizando Entity Framework Core. Se ha creado el contexto de la base de datos `ProyectoEcommerceContext` con un `DbSet` para productos.
    
    Se han definido los modelos `Product` y `ContactForm`, incluyendo validaciones. También se ha añadido la clase `Testimonial`.
    
    Se han creado las vistas `Create`, `Edit`, `Delete`, `Details` e `Index` para interactuar con los productos. Además, se han añadido archivos de configuración para las dependencias de la base de datos.

---
+++ ProyectoEcommerce/Data/ProyectoEcommerceContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProyectoEcommerce.Models;
+
+namespace ProyectoEcommerce.Data
+{
+    public class ProyectoEcommerceContext : DbContext
+    {
+        public ProyectoEcommerceContext (DbContextOptions<ProyectoEcommerceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProyectoEcommerce.Models.Product> Product { get; set; } = default!;
+    }
+}
---
+++ ProyectoEcommerce/Models/Testimonial.cs
@@ -0,0 +1,11 @@
+﻿namespace ProyectoEcommerce.Models
+{
+    public class Testimonial
+    {
+        public string Author { get; set; }
+        public string Comment { get; set; }
+
+
+
+    }
+}
